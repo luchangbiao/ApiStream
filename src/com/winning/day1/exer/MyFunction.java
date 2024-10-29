06eec86498c3670a00616e52e27aa7c1d59c1296
l_cb@winning.com.cn <lcb@8445741>
2024-10-29 13:58:16 +0800

    初始化apiStream

---
+++ src/com/winning/day1/exer/MyFunction.java
@@ -0,0 +1,8 @@
+package com.winning.day1.exer;
+
+@FunctionalInterface
+public interface MyFunction {
+	
+	public String getValue(String str);
+
+}
---
+++ src/com/winning/day2/java8/Status.java
@@ -0,0 +1,5 @@
+package com.winning.day2.java8;
+
+public enum Status {
+    FREE, BUSY, VOCATION;
+}
